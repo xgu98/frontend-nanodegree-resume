ea7fafe865f0548c0d6d56005ae5b65f234c01a5
gracegu <xgu@uchicago.edu>
2017-03-12 17:24:46 -0500

    Add bio object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,19 @@
-$("#main").append("Xiaohe Grace Gu");/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+
+var skills = ["programming", "leadership", "public speaking", "Microsoft Office"]
+
+
+var bio = {
+"name": "Xiaohe Grace Gu"
+"role": "Student"
+"contact info": "xgu@uchicago.edu"
+"picture URL": "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/16473573_749990395152636_1414885734714500220_n.jpg?oh=c4d8a0bbe0c5aa523a4c4312bafaa848&oe=59636907"
+"welcome message": "Welcome!"
+"skills" : skills
+
+}
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
