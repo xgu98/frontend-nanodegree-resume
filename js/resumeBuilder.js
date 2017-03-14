1da304ccd8bc5b236bc00907913a36a9a6ec9a8d
gracegu <xgu@uchicago.edu>
2017-03-13 21:44:02 -0500

    Add education display function and GoogleMap API

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,165 +2,214 @@
 
 
 var bio = {
-"name": "Xiaohe Grace Gu",
-"role": "Student",
-"contact info": {
-	"email":"xgu@uchicago.edu",
-	"mobile" :"312-838-8402",
-	"github" : "xgu98"
-},
-"picture URL": "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/16473573_749990395152636_1414885734714500220_n.jpg?oh=c4d8a0bbe0c5aa523a4c4312bafaa848&oe=59636907",
-"welcome message": "Welcome!",
-"skills" : ["programming", "leadership", "public speaking", "Microsoft Office"]
+	"name": "Xiaohe Grace Gu",
+	"role": "Student",
+	"contacts": {
+		"email":"xgu@uchicago.edu",
+		"mobile" :"312-838-8402",
+		"github" : "xgu98",
+		"location": "Chicago,IL"
+	},
+	"picture URL": "https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/16473573_749990395152636_1414885734714500220_n.jpg?oh=c4d8a0bbe0c5aa523a4c4312bafaa848&oe=59636907",
+	"welcome message": "Welcome!",
+	"skills" : ["programming", "leadership", "public speaking", "Microsoft Office"]
 
 };
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
 
-var work = {
+bio.display= function(){
 
-"jobs": [
+	var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	$("#header").append(formattedName);
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+	$("#header").append(formattedRole);
 
-{
-"title": "Research Assistant",
-"employer": "Becker Friedman Institute of Research in Economics",
-"dates": "November, 2016-present",
-"location": "Chicago, IL",
-"description" : "Conduct field experiments and manage research data on charitable giving and education policy for Economics Professor John A. List, a Nobel Prize candidate"
-},
+//$("#topContacts").append(HTMLcontactGeneric);
+var formattedEmail= HTMLemail.replace("%data%",bio.contacts.email);
+$("#topContacts").append(formattedEmail);
+var formattedMobile= HTMLmobile.replace("%data%",bio.contacts.mobile);
+$("#topContacts").append(formattedMobile);
+var formattedGithub= HTMLgithub.replace("%data%",bio.contacts.github);
+$("#topContacts").append(formattedGithub);
 
-{
-"title": "Reporting Intern",
-"employer": "36Kr",
-"dates": "January, 2017-present",
-"location": "Chicago, IL",
-"description" : "Report on startups in cutting-edge science and technology for Chinese audience interested in TMT news. Past articles covered Lithium-ion battery’s impact on EV industry and the future of energy, and organ-on-chip tech’s potential to optimize drug development and personalize medicine"
+if (bio.skills.length>0) {
+	
+	$("#header").append(HTMLskillsStart);
+	var formattedSkill=HTMLskills.replace("%data%", bio.skills[0]);
+	$("#skills").append(formattedSkill);
+	formattedSkill=HTMLskills.replace("%data%", bio.skills[1]);
+	$("#skills").append(formattedSkill);
+	formattedSkill=HTMLskills.replace("%data%", bio.skills[2]);
+	$("#skills").append(formattedSkill);
+	formattedSkill=HTMLskills.replace("%data%", bio.skills[3]);
+	$("#skills").append(formattedSkill);
 
 }
 
-]
+}
+var work = {
 
+	"jobs": [
+
+	{
+		"title": "Research Assistant",
+		"employer": "Becker Friedman Institute of Research in Economics",
+		"dates": "November, 2016-present",
+		"location": "Chicago, IL",
+		"description" : "Conduct field experiments and manage research data on charitable giving and education policy for Economics Professor John A. List, a Nobel Prize candidate"
+	},
+
+	{
+		"title": "Reporting Intern",
+		"employer": "36Kr",
+		"dates": "January, 2017-present",
+		"location": "Beijing, China",
+		"description" : "Report on startups in cutting-edge science and technology for Chinese audience interested in TMT news. Past articles covered Lithium-ion battery’s impact on EV industry and the future of energy, and organ-on-chip tech’s potential to optimize drug development and personalize medicine"
+
+	}
+
+	]
+
+};
+
+work.display= function(){
+	for(var i=0; i<work.jobs.length;i++){
+		$("#workExperience").append(HTMLworkStart);
+		var formattedEmployer=HTMLworkEmployer.replace("%data%",work.jobs[i].employer);
+		var formattedTitle=HTMLworkTitle.replace("%data%",work.jobs[i].title);
+		var formattedEmployerTitle= formattedEmployer+formattedTitle;
+		$(".work-entry:last").append(formattedEmployerTitle);
+		var formattedLocation=HTMLworkLocation.replace("%data%",work.jobs[i].location);
+		$(".work-entry:last").append(formattedLocation);
+		var formattedDates=HTMLworkDates.replace("%data%",work.jobs[i].dates);
+		$(".work-entry:last").append(formattedDates);
+		var formattedDescription=HTMLworkDescription.replace("%data%",work.jobs[i].description);
+		$(".work-entry:last").append(formattedDescription);
+	}
 };
 
 var education = {
 
-"schools" : [
+	"schools" : [
 
-{
-"name":"The University of Chicago",
-"city":"Chicago",
-"majors": ["Economics", "Computer Science"],
-"graduation year" : 2020
+	{
+		"name":"The University of Chicago",
+		"location":"Chicago, IL",
+		"majors": ["Economics", "Computer Science"],
+		"dates" : "September 2016-present",
+		"degree" : "Bachelor"
 
-},
+	},
 
-{
-"name":"Udacity",
-"city":"online",
-"majors": ["Frontend Web Developing"],
-"graduation year" : 2017
+	{
+		"name":"The Peddie School",
+		"location":"Hightstown, NJ",
+		"majors": [],
+		"dates" : "September 2012-May 2016",
+		"degree" : "High School"
 
-}
+	}
 
-]
+	]
+
+};
+
+education.display= function(){
+
+	$("#education").append(HTMLschoolStart);
+	for(var i=0; i<education.schools.length;i++){
+		var formattedName=HTMLschoolName.replace("%data%", education.schools[i].name);
+		$(".education-entry:last").append(formattedName);
+		var formattedDegree=HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+		$(".education-entry:last").append(formattedDegree);
+		var formattedDates=HTMLschoolDates.replace("%data%", education.schools[i].dates);
+		$(".education-entry:last").append(formattedDates);
+		var formattedLocation=HTMLschoolLocation.replace("%data%", education.schools[i].location);
+		$(".education-entry:last").append(formattedLocation);
+		if(education.schools[i].majors.length>0){
+
+			for(var j=0; j<education.schools[i].majors.length; j++){
+				var formattedMajor=HTMLschoolMajor.replace("%data%", education.schools[i].majors[j]);
+				$(".education-entry:last").append(formattedMajor);
+			}
+		}
+	}
+};
+
+var onlineClasses={
 
 };
 
 var projects={
 
 	"project":[
-{
-	"title":"Animal Trading Card",
-	"dates":"November 2016",
-	"description": "An animal trading card with American Red Squirrel description and image.",
-	"images": ["images/squirrel.jpeg"]
-},
+	{
+		"title":"Animal Trading Card",
+		"dates":"November 2016",
+		"description": "An animal trading card with American Red Squirrel description and image.",
+		"images": ["images/squirrel.jpeg"]
+	},
 
-{
-	"title":"Portfolio Site",
-	"dates":"December 2016",
-	"description": "A sample portfolio site with my profile picture and link to Peddie School's homepage",
-	"images": ["images/fry.jpg"]
-}
+	{
+		"title":"Portfolio Site",
+		"dates":"December 2016",
+		"description": "A sample portfolio site with my profile picture and link to Peddie School's homepage",
+		"images": ["images/fry.jpg"]
+	}
 
 
 
 	]
 };
 
- if (bio.skills.length>0) {
-	
-	$("#header").append(HTMLskillsStart);
-	var formattedSkill=HTMLskills.replace("%data%", bio.skills[0]);
-    $("#skills").append(formattedSkill);
-   formattedSkill=HTMLskills.replace("%data%", bio.skills[1]);
-    $("#skills").append(formattedSkill);
-    formattedSkill=HTMLskills.replace("%data%", bio.skills[2]);
-    $("#skills").append(formattedSkill);
-    formattedSkill=HTMLskills.replace("%data%", bio.skills[3]);
-    $("#skills").append(formattedSkill);
 
-}
-
-function displayWork(){
-for(var i=0; i<work.jobs.length;i++){
-	$("#workExperience").append(HTMLworkStart);
-	var formattedEmployer=HTMLworkEmployer.replace("%data%",work.jobs[i].employer);
-	var formattedTitle=HTMLworkTitle.replace("%data%",work.jobs[i].title);
-	var formattedEmployerTitle= formattedEmployer+formattedTitle;
-	$(".work-entry:last").append(formattedEmployerTitle);
-	var formattedLocation=HTMLworkLocation.replace("%data%",work.jobs[i].location);
-	$(".work-entry:last").append(formattedLocation);
-	var formattedDates=HTMLworkDates.replace("%data%",work.jobs[i].dates);
-	$(".work-entry:last").append(formattedDates);
-	var formattedDescription=HTMLworkDescription.replace("%data%",work.jobs[i].description);
-	$(".work-entry:last").append(formattedDescription);
-}
-};
-
-displayWork();
 
 $(document).click(function(loc) {
 	var x = loc.pageX;
 	var y = loc.pageY;
-  logClicks(x,y);
+	logClicks(x,y);
 });
 
 $("#main").append(internationalizeButton);
 function inName(name){
-name=name.trim().split(" ");
-name[0]=name[0].slice(0,1).toUpperCase()+name[0].slice(1).toLowerCase();
-name[1]=name[1].toUpperCase();
+	name=name.trim().split(" ");
+	name[0]=name[0].slice(0,1).toUpperCase()+name[0].slice(1).toLowerCase();
+	name[1]=name[1].toUpperCase();
 
-return name[0]+" "+name[1];
+	return name[0]+" "+name[1];
 }
 
 projects.display= function() {
 
-for(var i=0; i<projects.project.length;i++){
-	$("#projects").append(HTMLprojectStart);
-	var formattedTitle=HTMLprojectTitle.replace("%data%",projects.project[i].title);
-	$(".project-entry:last").append(formattedTitle);
-	var formattedDates=HTMLprojectDates.replace("%data%",projects.project[i].dates);
-	$(".project-entry:last").append(formattedDates);
-	var formattedDescription=HTMLprojectDescription.replace("%data%",projects.project[i].description);
-	$(".project-entry:last").append(formattedDescription);
+	for(var i=0; i<projects.project.length;i++){
+		$("#projects").append(HTMLprojectStart);
+		var formattedTitle=HTMLprojectTitle.replace("%data%",projects.project[i].title);
+		$(".project-entry:last").append(formattedTitle);
+		var formattedDates=HTMLprojectDates.replace("%data%",projects.project[i].dates);
+		$(".project-entry:last").append(formattedDates);
+		var formattedDescription=HTMLprojectDescription.replace("%data%",projects.project[i].description);
+		$(".project-entry:last").append(formattedDescription);
 
-	if(projects.project[i].images.length>0){
-		for(var j=0; j<projects.project[i].images.length;j++){
-		var formattedImage=HTMLprojectImage.replace("%data%", projects.project[i].images[j])
-		$(".project-entry:last").append(formattedImage);
+		if(projects.project[i].images.length>0){
+			for(var j=0; j<projects.project[i].images.length;j++){
+				var formattedImage=HTMLprojectImage.replace("%data%", projects.project[i].images[j])
+				$(".project-entry:last").append(formattedImage);
+			}
+		}
 	}
-	}
-}
 
 };
 
+
+
+bio.display();
+
+work.display();
+
+education.display();
+
 projects.display();
 
-
+$("#mapDiv").append(googleMap);
 
 
